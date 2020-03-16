3eb3462be14222ac4f428e92613105a79064c056
Sevenez <iamnhaaa@gmail.com>
2020-03-16 13:12:47 -0700

    Upload my project

---
+++ Hello.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "MathLib.h"
+using namespace std;
+
+int main(int argc, char **argv) {
+    cout << "Hello world" << endl;
+    cout << "Add(8, 11): " << Add(8, 11) << endl;
+    cout << "Sub(8, 11): " << Sub(8, 11) << endl;
+    cout << "Mul(8, 11): " << Mul(8, 11) << endl;
+    cout << "Div(8, 11): " << Div(8, 11) << endl;
+    return 0;
+}
+
---
+++ MathLib.h
@@ -0,0 +1,21 @@
+#ifndef _MATHLIB_H_
+#define _MATHLIB_H_
+
+int Add(int a, int b) {
+    return a + b;
+}
+
+int Sub(int a, int b) {
+    return a - b;
+}
+
+int Mul(int a, int b) {
+    return a * b;
+}
+
+int Div(int a, int b) {
+    return a / b;
+}
+
+#endif // _MATHLIB_H_
+
